32ca5a62907e08b9abf5571bb6354468d2484783
sebastiannyholm <sebn89@gmail.com>
2015-03-24 17:25:27 +0100

    Deleted NOOOO

--- Schedule/src/Driver.java
+++ Schedule/src/Driver.java
@@ -4,8 +4,6 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		
-		System.out.println("NOOOO");
 	}
 
 }
