c40d687180fb7108bf52fcdedbc07730a2d855d7
Toonwire <Toonwire@hotmail.com>
2015-03-24 18:40:03 +0100

    Added string s

--- Schedule/src/Driver.java
+++ Schedule/src/Driver.java
@@ -4,6 +4,9 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		String s = "hey";
+		System.out.println(s);
 	}
 
 }
