fe2513498b51524f277ad1a935718f5fb8d88bce
sebastiannyholm <sebn89@gmail.com>
2015-04-07 19:31:31 +0200

    Upstart for Tests

---
+++ Schedule/test/model/DataSetup.java
@@ -0,0 +1,21 @@
+package model;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+import org.junit.Before;
+
+public class DataSetup {
+		
+	@Before
+	public void setUp(){
+		
+	}
+	
+	@Test
+	public void check() {
+		
+	}
+	
+}
