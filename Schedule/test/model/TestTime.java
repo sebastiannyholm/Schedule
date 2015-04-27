1e4ea541b3efb8af0d93e94933034ae0d6c8bcfa
sebastiannyholm <sebn89@gmail.com>
2015-04-27 11:39:27 +0200

    getEmployee

--- Schedule/test/model/TestTime.java
+++ Schedule/test/model/TestTime.java
@@ -4,9 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.text.DateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
@@ -129,7 +127,7 @@ public class TestTime {
 
 		Project project = new Project("project!!022", week-4, week+6, schedule.getUser());
 		Task task1 = new Task("newTask", week-4, week+1, 200);		// within week agenda
-		Task task2 = new Task("popTask", week-2  , week-1, 100);	// within week agenda
+		Task task2 = new Task("popTask", week  , week + 1, 100);	// within week agenda
 		Task task3 = new Task("badTask", week+2, week+5, 80);		// outside week agenda
 		
 		user.createProject(project);
